95d4b16b18d05b5fd18a9e4fdf4c281ce7e9c85c
Justin <justing0210@gmail.com>
2026-03-03 23:34:46 +0800

    feat: created sample pages

---
+++ Client/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,13 @@
+import HeroSection from "./Sections/HeroSection"
+import PricingSection from "./Sections/PricingSection"
+
+const HomePage = () => {
+  return (
+    <>
+      <HeroSection />
+      <PricingSection />
+    </>
+  )
+}
+
+export default HomePage
