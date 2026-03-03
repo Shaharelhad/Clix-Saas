cb9d1a06a04a4023adf9ff1fce93604d4ec9b2f4
Justin <justing0210@gmail.com>
2026-03-03 23:34:56 +0800

    feat: created sample sections

---
+++ Client/src/pages/HomePage/Sections/HeroSection.tsx
@@ -0,0 +1,7 @@
+const HeroSection = () => {
+  return (
+    <div className="border-2 min-h-dvh flex items-center justify-center bg-black text-white text-xl">Hero Section</div>
+  )
+}
+
+export default HeroSection
