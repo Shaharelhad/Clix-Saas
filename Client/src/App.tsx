8d4a4b51024e8d566b4f0adbd003bdb7f654d3f4
Miko Basilio <151455386+okimchi123@users.noreply.github.com>
2026-03-05 00:59:47 +0800

    feat: integrated admin main page

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,19 +1,38 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import ProfilePage from "./pages/ProfilePage";
 import CreateBotPage from "./pages/CreateBotPage/CreateBotPage";
 import AuthPage from "./pages/AuthPage";
 import PendingPage from "./pages/PendingPage";
+import AdminGuard from "./components/AdminGuard";
+import AdminPage from "./pages/AdminPage/AdminPage";
+import AdminApprovalsSection from "./pages/AdminPage/Sections/AdminApprovalsSection";
+import AdminUsersSection from "./pages/AdminPage/Sections/AdminUsersSection";
+import FormBuilderSection from "./pages/AdminPage/Sections/FormBuilderSection";
 
 function App() {
   return (
     <Routes>
-      {/* Sample only */}
       <Route path="/" element={<HomePage />} />
       <Route path="/auth" element={<AuthPage />} />
       <Route path="/pending" element={<PendingPage />} />
       <Route path="/profile" element={<ProfilePage />} />
       <Route path="/create-bot" element={<CreateBotPage />} />
+
+      {/* Admin routes */}
+      <Route
+        path="/admin"
+        element={
+          <AdminGuard>
+            <AdminPage />
+          </AdminGuard>
+        }
+      >
+        <Route index element={<Navigate to="approvals" replace />} />
+        <Route path="approvals" element={<AdminApprovalsSection />} />
+        <Route path="users" element={<AdminUsersSection />} />
+        <Route path="form-builder" element={<FormBuilderSection />} />
+      </Route>
     </Routes>
   );
 }
