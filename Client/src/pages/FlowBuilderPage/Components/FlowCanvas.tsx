17fe5444907aaa5a04ab4cb74404a1ec4d3498e8
Justin <justing0210@gmail.com>
2026-03-09 22:18:03 +0800

    feat: updated FlowCanvas with AI agent node type

--- Client/src/pages/FlowBuilderPage/Components/FlowCanvas.tsx
+++ Client/src/pages/FlowBuilderPage/Components/FlowCanvas.tsx
@@ -20,6 +20,7 @@ import CollectInputNode from "./nodes/CollectInputNode";
 import DelayNode from "./nodes/DelayNode";
 import FollowUpNode from "./nodes/FollowUpNode";
 import ConditionNode from "./nodes/ConditionNode";
+import AiAgentNode from "./nodes/AiAgentNode";
 
 const nodeTypes: NodeTypes = {
   start: StartNode,
@@ -30,6 +31,7 @@ const nodeTypes: NodeTypes = {
   delay: DelayNode,
   follow_up: FollowUpNode,
   condition: ConditionNode,
+  ai_agent: AiAgentNode,
 };
 
 interface FlowCanvasProps {
